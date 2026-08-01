b279d7775401acbcaef954195827b980c3d94cc0
b3icedrage <uknownname1001@gmail.com>
2026-08-01 06:33:34 +0300

    Fix Snap X feed upload button

--- supabase/client.js
+++ supabase/client.js
@@ -2,7 +2,7 @@ import { createClient } from
 "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 
-const supabaseUrl = "https://socaxjkikrxantwxacqy.supabase.co/rest/v1/";
+const supabaseUrl = "https://socaxjkikrxantwxacqy.supabase.co";
 
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvY2F4amtpa3J4YW50d3hhY3F5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1MTMzNzEsImV4cCI6MjEwMTA4OTM3MX0.wBhxPka_CsqSVTQHjr3DS2ddvw_ezht4NToqTbzGzb4";
 
